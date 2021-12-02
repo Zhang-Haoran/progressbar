21f1dfef32ce9213c1c04dcab98c3e607259e56c
Zhang-Haoran <haoranzhang001@gmail.com>
2021-12-02 16:20:40 +1100

    updated

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ProgressBarData} from './progress-bar.model';
 
 @Component({
